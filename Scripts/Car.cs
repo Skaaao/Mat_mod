6d99fe8142c6314d21c3e3db5b64ef79f130569a
Skaaao <77786826+Skaaao@users.noreply.github.com>
2021-02-11 12:25:32 +0300

    Add files via upload

---
+++ Scripts/Car.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    public bool inWindZone = false;
+    public GameObject windZone;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (this.transform.position.y <= -1)
+        {
+            Destruction();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (inWindZone)
+        {
+            rb.AddForce(windZone.GetComponent<WindArea>().direction * windZone.GetComponent<WindArea>().strength);
+        }
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == "windArea")
+        {
+            windZone = coll.gameObject;
+            inWindZone = true;
+        }
+    }
+
+    void OnTriggerExit(Collider coll)
+    {
+        if (coll.gameObject.tag == "windArea")
+        {
+            inWindZone = false;
+        }
+    }
+
+    void OnCollision(Collision coll)
+    {
+        if (coll.gameObject.name == "destroyer")
+        {
+            Destruction();
+        }
+    }
+
+    void Destruction()
+    {
+        Destroy(this.gameObject);
+    }
+}
